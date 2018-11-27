082223b97e159d0ba6abd2bf6f5f2d19a6ff8485
gabrielalvarez <gabrielalvarez@dorna.com>
2018-11-27 10:57:16 +0100

    method child

--- components/parent-data.js
+++ components/parent-data.js
@@ -3,7 +3,7 @@ Vue.component('parent-data',{
     template: `
     <div>
         <h2>Acceso a datos del cmp padre desde el cm hijo</h2>
-        <p>{{ $parent.appName}}</p>
+        <p>{{ $parent.appName }}</p>
     </div>
     `
 } )
