ae688e4dc5f16c28fb11d7c7c34f4c6f342d5af3
Gabriel Alvarez <gabrielalvarez@dorna.com>
2018-11-27 16:15:47 +0100

    form

---
+++ components/login.js
@@ -0,0 +1,30 @@
+Vue.component('login-form', {
+    data() {
+        return {
+            logged: false,
+            user: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        login() {
+            this.logged = this.user.email === 'test@m.com' && this.user.password === '1234';
+        }
+    },
+    template:`
+    <div>
+        <h2>Formulario Login</h2>
+        <p v-show="logged" style="background: green; color: #fff">
+            Has iniciado sesión con los datos : {{ user }}
+        </p>
+        <form @submit.prevent="login">
+        <input autocomplete="off" type="email" v-model="user.email" name="email">
+        <input type="password" v-model="user.password" name="password">
+        <input type="submit" name="email" value="Iniciar sesión">
+
+        </form>
+    </div>
+    `
+})
